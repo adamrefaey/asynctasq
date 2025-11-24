40963246235093cf33fd830a920e04baab158a00
Adam Refaey <dev.adam.refaey@gmail.com>
2025-11-24 08:32:26 +0200

    Add PostgresDriver implementation for PostgreSQL-based task queue

--- src/async_task/drivers/__init__.py
+++ src/async_task/drivers/__init__.py
@@ -6,12 +6,14 @@ for various queue backends (memory, Redis, AWS SQS).
 
 from .base_driver import BaseDriver
 from .memory_driver import MemoryDriver
+from .postgres_driver import PostgresDriver
 from .redis_driver import RedisDriver, maybe_await
 from .sqs_driver import SQSDriver
 
 __all__ = [
     "BaseDriver",
     "MemoryDriver",
+    "PostgresDriver",
     "RedisDriver",
     "SQSDriver",
     "maybe_await",
