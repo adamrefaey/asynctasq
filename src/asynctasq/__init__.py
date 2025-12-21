1c443a720704369923dbe7683e5c2c7cabbb99ec
Adam Refaey <dev.adam.refaey@gmail.com>
2025-12-21 03:50:10 +0200

    chore: add version retrieval logic to __init__.py

--- src/asynctasq/__init__.py
+++ src/asynctasq/__init__.py
@@ -1,1 +1,8 @@
 """AsyncTasQ - Modern async-first task queue for Python."""
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("asynctasq")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
