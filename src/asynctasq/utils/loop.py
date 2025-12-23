4a4701987d8c3bc57119247dc88c7049a0620064
Adam Refaey <dev.adam.refaey@gmail.com>
2025-12-23 13:19:33 +0200

    refactor: improve cleanup process in run() by ensuring AsyncTasQ resources are properly released

--- src/asynctasq/utils/loop.py
+++ src/asynctasq/utils/loop.py
@@ -13,6 +13,19 @@ import uvloop
 logger = logging.getLogger(__name__)
 
 
+async def _cleanup_asynctasq():
+    """Cleanup AsyncTasQ resources if initialized."""
+    try:
+        from asynctasq.core.dispatcher import cleanup
+
+        await cleanup()
+    except TimeoutError:
+        logger.warning("AsyncTasQ cleanup timed out")
+    except Exception:
+        # Ignore cleanup errors
+        pass
+
+
 def run(coro: Any):
     """Run coroutine using a fresh uvloop event loop.
 
@@ -23,6 +36,8 @@ def run(coro: Any):
     try:
         return loop.run_until_complete(coro)
     finally:
+        # Cleanup AsyncTasQ resources before shutting down asyncgens
+        loop.run_until_complete(_cleanup_asynctasq())
         try:
             loop.run_until_complete(loop.shutdown_asyncgens())
         finally:
